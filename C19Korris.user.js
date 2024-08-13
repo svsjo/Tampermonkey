22ae6fabcce0a7e16aadcbc449b78a96f107a431
svsjo <79276639+svsjo@users.noreply.github.com>
2024-08-13 11:33:24 +0200

    Update C19Korris.user.js

--- C19Korris.user.js
+++ C19Korris.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         C19 Korris WInkel
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://dep19.die-staemme.de/*screen=map*
